54f08addd39557618b14abc980c467db46607163
juwon ayorinde jaceayo123@gmail.com <jaceayo123@gmail.com>
2023-09-05 14:13:54 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+users = User.create([{ email: "kanyecrazy2023@gmail.com"}, {encrypted_password: "12345678"}, {username:"@Kanye_crzy" },
+{bio:" - All Gas No Breaks - Upcoming artist with sick beeeats too ,yoo! - Rapper/Beats"},{location: "Chicago,Illinois"}]),
+
+users = User.create([{ email: "yungthugga022@gmail.com"}, {encrypted_password:"12345678"}, {username:"@Youngthug" },
+{bio:" I’m the type of person to put myself in everybody else’s shoes - Rapper"},{location: "Thug's World"}]),
+
+users = User.create([{ email: "1future@gmail.com"}, {encrypted_password:"12345678"}, {username:"@PLUTOHENDRIX" },
+{ bio:"Purple Reign - Rapper " },{location: " Atlanta, Georgia, "}]),
+
+users = User.create([{ email: "TayKeithrosssata@hotmail.com"}, {encrypted_password:"12345678"}, {username:"@Tay_Keith" },
+{bio:" - Get it done. Ain’t no excuses - Producer"}, {location: "Memphis, TN"}])
+
+users = User.create([{ email: "Bad_Bunny@hotmail.com"}, {encrypted_password:"12345678"}, {username:"@BaDBuNNy" },
+{bio:" - Antes' de que salga el sol, hunde el acelerador - Singer"}, {location: "San Jose, Costa Rica"}])
+
+posts = Post
