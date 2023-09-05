85cfc3d2c029b37de6cf0541c082b2d0f39529d2
Ryuhei Kawamura <ryan.or.ryuhei@gmail.com>
2023-09-05 14:03:34 +0200

    add posts page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,27 +1,34 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
-  end
-
-  def show
+    @posts = Post.includes(:user).order(updated_at: :desc)
+    @like = Like.new
   end
 
   def new
     @post = Post.new
-  end
-
-  def edit
+    authorize @post
   end
 
   def create
     @post = Post.new(post_params)
-
+    @post.user = current_user
+    @post.group = post_params[:group] if params[:group].present?
+    authorize @post
     if @post.save
-      redirect_to @post, notice: 'Post was successfully created.'
+      if @post.temp_gif_url.present?
+        file = URI.open(@post.temp_gif_url)
+        @post.photos.attach(io: file, filename: 'some-image.gif', content_type: 'image/gif')
+      end
+      redirect_to request.referrer
+      # redirect_to feed_users_path
     else
       render :new
     end
   end
 
-  
+  private
+
+  def post_params
+    params.require(:post).permit(:content, :user, :group, :temp_gif_url, photos: [])
+  end
 end
