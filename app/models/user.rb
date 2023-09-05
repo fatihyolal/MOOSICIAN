c5798c70cad658d2be861eca53d9fd5d849dc0fe
dhiraj chakma <chakmadhiraj@gmail.com>
2023-09-05 15:39:27 +0200

    footer

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,6 @@ class User < ApplicationRecord
   has_many :messages, dependent: :destroy
   has_many :chatrooms, through: :messages
 
-  validates :username, presence: true, uniqueness: true
-  validates :bio, presence: true
+  # validates :username, presence: true, uniqueness: true
+  # validates :bio, presence: true
 end
