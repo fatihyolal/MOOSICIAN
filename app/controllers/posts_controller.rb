03e75848ab0262d36d6eee4e6b59e346c9124cad
Ryuhei Kawamura <ryan.or.ryuhei@gmail.com>
2023-09-05 16:00:13 +0200

    add posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,34 +1,24 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.includes(:user).order(updated_at: :desc)
-    @like = Like.new
-  end
-
-  def new
+    @posts = Post.all
     @post = Post.new
-    authorize @post
   end
 
   def create
     @post = Post.new(post_params)
-    @post.user = current_user
-    @post.group = post_params[:group] if params[:group].present?
-    authorize @post
     if @post.save
-      if @post.temp_gif_url.present?
-        file = URI.open(@post.temp_gif_url)
-        @post.photos.attach(io: file, filename: 'some-image.gif', content_type: 'image/gif')
+      respond_to do |format|
+        format.html { redirect_to posts_path }
+        format.js
       end
-      redirect_to request.referrer
-      # redirect_to feed_users_path
     else
-      render :new
+      render :index
     end
   end
 
   private
 
   def post_params
-    params.require(:post).permit(:content, :user, :group, :temp_gif_url, photos: [])
+    params.require(:post).permit(:title, :description, :category)
   end
 end
