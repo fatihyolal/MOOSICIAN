7875dcbe4fdc1b7af244c49861409347bce20e44
Ryuhei Kawamura <ryan.or.ryuhei@gmail.com>
2023-09-05 18:01:17 +0200

    add posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,19 +6,21 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
+    @post.user = current_user
     if @post.save
       respond_to do |format|
         format.html { redirect_to posts_path }
         format.js
       end
     else
-      render :index
+      @posts = Post.all
+      render :index, status: :unprocessable_entity
     end
   end
 
   private
 
   def post_params
-    params.require(:post).permit(:title, :description, :category)
+    params.require(:post).permit(:title, :description, :category, :attachment)
   end
 end
