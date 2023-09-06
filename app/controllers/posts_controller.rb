20b4d8f6f1495aee7b29075fe0d00be022cd64e7
Ryuhei Kawamura <ryan.or.ryuhei@gmail.com>
2023-09-06 15:26:11 +0200

    add attached file icon

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,4 +5,18 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      respond_to do |format|
+        format.html { redirect_to posts_path }
+        format.js
+      end
+    else
+      @posts = Post.all
+      render :index, status: :unprocessable_entity
+    end
+  end
+
 end
