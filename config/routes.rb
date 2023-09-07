d0e9a4ea4025b01441469bbc91dff677cbaa267d
Ryuhei Kawamura <ryan.or.ryuhei@gmail.com>
2023-09-07 11:48:22 +0200

    fixing the new post

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :posts, only: :index
+  resources :posts, only: [:index]
   resources :users, only: [:show]
 end
